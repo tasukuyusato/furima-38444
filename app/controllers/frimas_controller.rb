ac83791dc57d56d4cc8071ee77613672edd69ee0
yusato <yu.sato0615@gmail.com>
2022-12-07 18:53:11 +0900

    index viewの表示

---
+++ app/controllers/frimas_controller.rb
@@ -0,0 +1,6 @@
+class FrimasController < ApplicationController
+
+ def index
+ end
+
+end
---
+++ test/controllers/frimas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FrimasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
