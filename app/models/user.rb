8ea644e21b8b3890aa69ea3b08c8f063b31559b2
yusato <yu.sato0615@gmail.com>
2022-12-19 23:53:59 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates  :nickname,         presence: true  
+  validates  :first_name,       presence: true  
+  validates  :last_name,        presence: true        
+  validates  :first_name_kana,  presence: true, format: { with: /\A[ｧ-ﾝﾞﾟァ-ヶー－）]+\z/ }          
+  validates  :last_name_kana,   presence: true, format: { with: /\A[ｧ-ﾝﾞﾟァ-ヶー－）]+\z/ }           
+  validates  :birthday,         presence: true     
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+end
+
+
+
